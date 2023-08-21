5367448b341742808d20bc2b2919991c94c3a9ff
Luchi <luciamilagroangiolini@gmail.com>
2023-08-21 15:18:41 -0300

    commit para main de raices

---
+++ raices_main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Luchi on 8/8/2023.
+//
+#include "raices.h"
+#include <iostream>
+using namespace std;
+int main(){
+    biseccion();
+    punto_fijo();
+    newton_raphson();
+    secante();
+}
